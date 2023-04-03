a7f5148a6830b28cede939412e5671a383927223
gaziozgen <gaziozgen98@gmail.com>
2023-04-03 14:13:20 +0300

    tap to speed up update

--- Assets/Game/TapToSpeedUp/TapToSpeedUp.cs
+++ Assets/Game/TapToSpeedUp/TapToSpeedUp.cs
@@ -2,27 +2,38 @@ using FateGames.Core;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.EventSystems;
 using UnityEngine.UIElements;
 
 public class TapToSpeedUp : MonoBehaviour
 {
     [SerializeField] private FloatReference targetVariable;
-    [SerializeField] GameEvent soldierGlow;
+    [SerializeField] GameEvent onTappedToSpeedUp;
+    [SerializeField] private Canvas canvas;
     [SerializeField] private float impactDuration = 1.5f;
     [SerializeField] private float multiplier = 2;
 
     private float bonusEndTime = -1;
+    private bool IsInGame = false;
 
     private void Start()
     {
+        Init();
+    }
+
+    private void Init()
+    {
         targetVariable.Value = 1;
+        void TapCheck() { if (!EventSystem.current.IsPointerOverGameObject() || EventSystem.current.currentSelectedGameObject == null) Tap(); }
         InputManager.GetKeyDownEvent(KeyCode.Mouse0).AddListener(TapCheck);
     }
 
+
     void Update()
     {
-        UpdateMultiplier();
+        if (IsInGame)
+            UpdateMultiplier();
     }
 
     private void UpdateMultiplier()
@@ -30,25 +41,36 @@ public class TapToSpeedUp : MonoBehaviour
         if (Time.time < bonusEndTime)
         {
             if (targetVariable.Value == 1)
+            {
                 targetVariable.Value = multiplier;
+                canvas.enabled = false;
+            }
         }
         else
         {
-            if (targetVariable.Value == 2)
+            if (targetVariable.Value == multiplier)
+            {
                 targetVariable.Value = 1;
+                canvas.enabled = true;
+            }
         }
-            
-    }
-
-    private void TapCheck()
-    {
-        if (EventSystem.current.IsPointerOverGameObject() && EventSystem.current.currentSelectedGameObject != null) return;
-        Tap();
     }
 
     public void Tap()
     {
-        soldierGlow.Raise();
+        onTappedToSpeedUp.Raise();
         bonusEndTime = Time.time + impactDuration;
+    }
+
+    public void OnGameStart()
+    {
+        IsInGame = true;
+        canvas.enabled = true;
+    }
+
+    public void OnGameEnd()
+    {
+        IsInGame = false;
+        canvas.enabled = false;
     }
 }
